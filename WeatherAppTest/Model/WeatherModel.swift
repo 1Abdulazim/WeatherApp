ddcc06a5058d2f93ddc5b0a3e43cd84f85bb5316
1Abdulazim <a.muhammadrafiqov@student.inha.uz>
2021-04-22 10:14:41 +0500

    first Commit

---
+++ WeatherAppTest/Model/WeatherModel.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherModel.swift
+//  WeatherAppTest
+//
+//  Created by Abdulazim on 08/02/21.
+//
+
+import Foundation
---
+++ WeatherAppTest/SearchCountryView/Cell/CountryTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CountryTableViewCell.swift
+//  WeatherAppTest
+//
+//  Created by Abdulazim on 08/02/21.
+//
+
+import UIKit
+
+class CountryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
